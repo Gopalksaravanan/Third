7aa95d1fbd0e33b7be271e6a95434ead2284ab6d
Gopalksaravanan <sgopalmechanic@gmail.com>
2022-11-09 11:42:11 +0530

    message

---
+++ src/main/java/org/third/GidThird.java
@@ -0,0 +1,20 @@
+package org.third;
+
+public class GidThird {
+
+	public void method1() {
+		System.out.println("First");
+	}
+	
+	public void method2() {
+		System.out.println("Second");
+	}
+	public void method3() {
+		System.out.println("Third");
+	}
+	
+	
+	
+	
+	
+}
